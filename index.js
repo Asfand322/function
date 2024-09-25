a19a6a4ce1e03fc5c90bd98fe95805d9dfffff5b
Asfand322 <asfandsheikh321@gmail.com>
2024-09-25 13:53:48 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function ali() {
+    let stdtm = prompt("enter your 1st number")
+    let stdom = prompt("enter your 2st number")
+    let res = (stdom/stdtm)* 100
+    alert("you have got it" + res + "%")
+    console.log("you have got it" + res + "%")
+}
+ali()
+ali()
+ali()
+ali()
